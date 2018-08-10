65a35936d24fa278fb3f7748cb9d472d6cb28dda
Jose Soto <ezesoto13@gmail.com>
2018-08-10 11:16:45 -0600

    endpoints to compare images changed

--- src/components/dialog/dialog.js
+++ src/components/dialog/dialog.js
@@ -8,7 +8,7 @@ import Slide from '@material-ui/core/Slide';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { ADD_OBJECT, COMPARE_FACES, CREATE_BUCKET } from "../../endpoints";
+import { COMPARE_FACES_BASE64 } from "../../endpoints";
 
 class AlertDialogSlide extends React.Component {
   constructor(props) {
@@ -23,54 +23,10 @@ class AlertDialogSlide extends React.Component {
   handleClose = () => {
     this.setState({ open: false });
   };
-  
-  createBucket = (bucketName, sourceName, targetName) => {
-    const body = JSON.stringify({ bucketName });
 
-    fetch(CREATE_BUCKET, { method: 'POST', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', }, body })
-      .then((res) => res.json())
-      .then((bucket) => {
-        this.createObjectSource(bucketName, sourceName, targetName);
-        return bucket;
-      })
-      .catch((err) => {
-        return err;
-      })
-  }
-
-  createObjectSource = (bucketName, sourceName, targetName) => {
-    const source = 'user.jpg';
-    const bodySource = JSON.stringify({ bucketName, keyName: source, fileBase64: sourceName });
-
-    fetch(ADD_OBJECT, { method: 'POST', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', }, body: bodySource })
-      .then((res) => res.json())
-      .then((object) => {
-        this.createObjectTarget(bucketName, source, targetName);
-        return object;
-      })
-      .catch((err) => {
-        return err;
-      })
-  }
-
-  createObjectTarget = (bucketName, sourceName, targetName) => {
-    const target = 'cedula.jpg';
-    const bodyTarget = JSON.stringify({ bucketName, keyName: target, fileBase64: targetName });
-
-    fetch(ADD_OBJECT, { method: 'POST', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', }, body: bodyTarget })
-      .then((res) => res.json())
-      .then((object) => {
-        this.compareFaces(bucketName, sourceName, target);
-        return object;
-      })
-      .catch((err) => {
-        return err;
-      })
-  }
-
-  compareFaces = (bucketName, sourceName, targetName) => {
-    const body = JSON.stringify({ bucketName, sourceName, targetName });
-    fetch(COMPARE_FACES, { method: 'post', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', }, body })
+  compareFaces = (sourceImage, targetImage) => {
+    const body = JSON.stringify({ sourceImage, targetImage });
+    fetch(COMPARE_FACES_BASE64, { method: 'post', headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', }, body })
       .then((res) => res.json())
       .then((faceCompared) => {
         if (faceCompared.FaceMatches.length > 0) {
@@ -95,7 +51,7 @@ class AlertDialogSlide extends React.Component {
     imageTarget = imageTarget.replace("data:image/jpeg;base64,", '');
 
     // console.log(imageSource, imageTarget)
-    this.createBucket("userfake2018", imageSource, imageTarget);
+    this.compareFaces(imageSource, imageTarget);
     this.setState({ open: false, sourceImage: imageSource, targetImage, imageTarget });
   }
     
